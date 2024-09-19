d5aae3066c9b8339ca5175afc1dbb00c27f510b7
reactexcel <arun.etech2011@gmail.com>
2024-09-19 15:28:57 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("express")();
 const server = require("http").createServer(app);
+const cors = require("cors");
 
 const io = require("socket.io")(server, {
 	cors: {
@@ -8,6 +9,7 @@ const io = require("socket.io")(server, {
 	}
 });
 
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
